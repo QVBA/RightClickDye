f1f8b4db6fdece8ed5995fc0515b10d9bb5b30b6
QVBA <littlekicker111@outlook.com>
2016-04-09 04:31:42 +0100

    Update PlayerRightClick.java
    Fixed dyes being removed from inventory when dyeing wool of the same colour.
    
    Fixed dye being taken even when dyeing the same colour wool.

--- src/main/java/com/github/QVBA/Events/PlayerRightClick.java
+++ src/main/java/com/github/QVBA/Events/PlayerRightClick.java
@@ -25,11 +25,11 @@ public class PlayerRightClick {
 		
 		ItemDye dye = (ItemDye)itemUse.getItem();
 		BlockColored block = (BlockColored) clickedBlock;
+		
 		for(ForgeDirection side : ForgeDirection.VALID_DIRECTIONS) {
-			block.recolourBlock(event.world, event.x, event.y, event.z, side, BlockColored.func_150032_b(dye.getDamage(itemUse)));
+			if(block.recolourBlock(event.world, event.x, event.y, event.z, side, BlockColored.func_150032_b(dye.getDamage(itemUse)))) {
+				event.entityPlayer.inventory.consumeInventoryItem(dye);
+			}
 		}
-		event.entityPlayer.inventory.consumeInventoryItem(dye);
 	}
-	
-
 }
