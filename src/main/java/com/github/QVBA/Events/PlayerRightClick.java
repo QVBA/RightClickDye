4649779596b3a25862dec75ab0162daa8dda8ce4
QVBA <littlekicker111@outlook.com>
2016-04-10 13:04:41 +0100

    Stained_Glass_and_Carpets_can_now_be_dyed

--- src/main/java/com/github/QVBA/Events/PlayerRightClick.java
+++ src/main/java/com/github/QVBA/Events/PlayerRightClick.java
@@ -2,13 +2,16 @@ package com.github.QVBA.Events;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockCarpet;
 import net.minecraft.block.BlockColored;
+import net.minecraft.block.BlockStainedGlass;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemDye;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.entity.player.PlayerUseItemEvent;
@@ -17,19 +20,25 @@ public class PlayerRightClick {
 	
 	@SubscribeEvent
 	public void onEvent(PlayerInteractEvent event) {
+		
+		int x = event.x;
+		int y = event.y;
+		int z = event.z;
 		EntityPlayer player = event.entityPlayer;
 		ItemStack itemUse = player.getHeldItem();
-		if(itemUse == null || itemUse.getItem() != Items.dye) return;
 		Block clickedBlock = event.world.getBlock(event.x, event.y, event.z);
-		if(clickedBlock != Blocks.wool) return;
+		World world = event.world;
 		
-		ItemDye dye = (ItemDye)itemUse.getItem();
-		BlockColored block = (BlockColored) clickedBlock;
+		if(itemUse == null || itemUse.getItem() != Items.dye) return;
+		if(clickedBlock != Blocks.wool && clickedBlock != Blocks.carpet && clickedBlock != Blocks.stained_glass) return;
 		
-		for(ForgeDirection side : ForgeDirection.VALID_DIRECTIONS) {
-			if(block.recolourBlock(event.world, event.x, event.y, event.z, side, BlockColored.func_150032_b(dye.getDamage(itemUse)))) {
-				event.entityPlayer.inventory.consumeInventoryItem(dye);
-			}
+		int color = BlockColored.func_150032_b(((ItemDye)itemUse.getItem()).getDamage(itemUse));
+		int meta = world.getBlockMetadata(x, y, z);
+		if(meta != color) {
+			world.setBlockMetadataWithNotify(x, y, z, color, 3);
+			player.inventory.consumeInventoryItem(itemUse.getItem());
 		}
+		
+		
 	}
 }
